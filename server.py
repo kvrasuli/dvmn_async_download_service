f74a8e56753e750b3f544b4edf6b0910f9fd6455
Kirill Rasuli <rasulikv@gmail.com>
2020-09-12 17:22:38 +0300

    Add command line arguments

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import aiofiles
 import asyncio
 import os
 import logging
+import argparse
 
 
 logger = logging.getLogger(__file__)
@@ -14,11 +15,12 @@ async def archivate(request):
     response.headers['Content-Type'] = 'multipart/form-data'
     response.headers['Content-Disposition'] = f'attachment;filename={archive_hash}.zip'
 
-    if not os.path.exists(f'test_photos/{archive_hash}/'):
+    path = os.path.join(app.path, archive_hash)
+    if not os.path.exists(path):
         raise web.HTTPNotFound(text='Archive doesn\'t exist or was deleted.')
 
     archiving_process = await asyncio.create_subprocess_exec(
-        'zip', '-j', '-r', '-', f'test_photos/{archive_hash}/',
+        'zip', '-j', '-r', '-', path,
         stdout=asyncio.subprocess.PIPE
     )
     await response.prepare(request)
@@ -29,7 +31,7 @@ async def archivate(request):
                 break
             logger.info('Sending archive chunk ...')
             await response.write(archive_chunk)
-            await asyncio.sleep(1)
+            await asyncio.sleep(app.delay)
     except asyncio.CancelledError:
         logger.debug('Download was interrupted!')
         raise
@@ -45,11 +47,31 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--log', help='Enable logging', action='store_true'
+    )
+    parser.add_argument(
+        '--delay', help='Enable answer delay', default=0, type=int
+    )
+    parser.add_argument(
+        '--path', help='Path to photos folder', default='test_photos'
+    )
+    args = parser.parse_args()
+    return args.log, args.delay, args.path
+
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    log, delay, path = parse_args()
+    if log:
+        logging.basicConfig(level=logging.DEBUG)
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archivate),
     ])
+    app.path = path
+    app.delay = delay
     web.run_app(app)
